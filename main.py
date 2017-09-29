82787f92ba9790fa44c680c87d0b214a1812005b
Lance Liu <lanceknight@users.noreply.github.com>
2017-09-29 13:12:38 -0500

    Book3

--- main.py
+++ main.py
@@ -1,11 +1,12 @@
 
-for i in range(1,4):
+for i in range(1,60):
     lesson_no = str(i)
     lesson_dir = "data/" +lesson_no + ".txt"
     with open("data/total.txt","a") as output:
 
         with open(lesson_dir, "r") as input:
             text = input.read()
+            output.write("\n")
             output.write("======Lesson: " + lesson_no + "======\n" )
             output.write(text)
             output.write("\n")
